65261b83278e517f1943d3dad5b74e8cf37964a1
Vorcakova <julia.vorcakova@accenture.com>
2019-10-10 08:59:46 +0200

    fix bugs

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -2,6 +2,8 @@ const initialState = {
     counter : 0
 }
 
-const reducer = () => {
-    return state
-}
+const reducer = (state= initialState,action) => {
+    return state;
+};
+
+export default reducer;
