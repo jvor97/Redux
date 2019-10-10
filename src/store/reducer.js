3376e406f73fcdbd994a15923945a636787f0208
Vorcakova <julia.vorcakova@accenture.com>
2019-10-10 13:13:52 +0200

    switch insted of IF

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,29 +1,28 @@
 const initialState = {
-    counter : 0
-}
+  counter: 0
+};
 
-const reducer = (state= initialState,action) => {
-    if (action.type === 'INCREMENT') {
-        return {
-        counter: state.counter +1
-        }
-    }
-    if (action.type === 'DECREMENT') {
-        return {
-        counter: state.counter -1
-        }
-    }
-    if (action.type === 'ADD') {
-        return {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "INCREMTN":
+      return {
+        counter: state.counter + 1
+      };
+    case "DECREMENT":
+      return {
+        counter: state.counter - 1
+      };
+    case "ADD":
+      return {
         counter: state.counter + action.value
-        }
-    }
-    if (action.type === 'SUBTRACT') {
-        return {
+      };
+    case "SUBTRACT":
+      return {
         counter: state.counter - action.value
-        }
-    }
-    return state;
+      };
+  }
+  return state;
+  //IMPORTANT: return state has to be defined, bc if there is no 'right case', it would fall. But now it will return prev state
 };
 
 export default reducer;
